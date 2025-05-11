21b814ebeacbf9fc6f2a01c1621bf6b0fe021c6c
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-05-12 00:01:11 +0200

    [UPD] array done

--- src/App.jsx
+++ src/App.jsx
@@ -25,22 +25,7 @@ function App() {
 
   return (
     <>
-      <div className="container">
-        <h1>REACT FORM</h1>
-        <ul className="list-group mt-4">
-          {articoli.map((articolo) => {
-            return <li key={articolo.id} className='list-group-item'>
-              {articolo.titolo}
-            </li>
-          })}
-        </ul>
-        <form className='d-flex mt-4'>
-          <div className="input-group">
-            <input type="text" className='form-control' placeholder='Nuovo Titolo' />
-          </div>
-          <button className="btn btn-primary">Aggiungi</button>
-        </form>
-      </div>
+      <h1>FORM REACT</h1>
     </>
   )
 }
